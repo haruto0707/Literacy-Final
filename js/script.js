359bca14346a93d4006effc95dd451621378a4f8
haruto0707 <221205070@ccmailg.meijo-u.ac.jp>
2023-07-07 12:13:33 +0900

    index pre-commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
-window.addEventListener("DOMContentLoaded", function() {
-    var header = document.querySelector(".header");
-    header.style.zIndex = "9999";
+document.addEventListener("DOMContentLoaded", function () {
+    const modalBtns = document.querySelectorAll(".modalOpen");
+    modalBtns.forEach(function (btn) {
+        btn.onclick = function () {
+            var modal = btn.getAttribute('data-modal');
+            document.getElementById(modal).style.display = "block";
+        };
+    });
+    const closeBtns = document.querySelectorAll(".modalClose");
+    closeBtns.forEach(function (btn) {
+        btn.onclick = function () {
+            var modal = btn.closest('.modal');
+            modal.style.display = "none";
+        };
+    });
+
+    window.onclick = function (event) {
+        if (event.target.className === "modal") {
+            event.target.style.display = "none";
+        }
+    };
+    var videoHeight = document.getElementById('video').offsetHeight;
+    document.querySelector('.main').style.marginTop = videoHeight + 'px';
 });
+
+const CLASSNAME = "-visible";
+const TIMEOUT = 100;
+const $target = $(".bg");
+
+setInterval(() => {
+    $target.addClass(CLASSNAME);
+
+}, TIMEOUT * 2);
